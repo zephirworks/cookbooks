005688689cc6eab5a856965b4b8eb68ec2d927e3
Andrea Campi <andrea.campi@zephirworks.com>
2011-09-19 23:13:01 +0200

    [COOK-634] Convert unicorn recipe from a recipe to a LWRP.

---
+++ application/providers/unicorn.rb
@@ -0,0 +1,59 @@
+#
+# Cookbook Name:: application
+# Recipe:: unicorn
+#
+# Copyright 2011, Opscode, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+action :create do
+  app = new_resource.application
+
+  run_context.include_recipe "unicorn"
+
+  node.default[:unicorn][:worker_timeout] = 60
+  node.default[:unicorn][:preload_app] = false
+  node.default[:unicorn][:worker_processes] = [node[:cpu][:total].to_i * 4, 8].min
+  node.default[:unicorn][:preload_app] = false
+  node.default[:unicorn][:before_fork] = 'sleep 1' 
+  node.default[:unicorn][:port] = '8080'
+  node.set[:unicorn][:options] = { :tcp_nodelay => true, :backlog => 100 }
+
+  unicorn_config "/etc/unicorn/#{app['id']}.rb" do
+    listen({ node[:unicorn][:port] => node[:unicorn][:options] })
+    working_directory ::File.join(app['deploy_to'], 'current')
+    worker_timeout node[:unicorn][:worker_timeout] 
+    preload_app node[:unicorn][:preload_app] 
+    worker_processes node[:unicorn][:worker_processes]
+    before_fork node[:unicorn][:before_fork] 
+  end
+
+  runit_service app['id'] do
+    template_name 'unicorn'
+    cookbook 'application'
+    options(
+      :app => app,
+      :rails_env => node.run_state[:rails_env] || node.chef_environment,
+      :smells_like_rack => ::File.exists?(::File.join(app['deploy_to'], "current", "config.ru"))
+    )
+    run_restart false
+  end
+
+  if ::File.exists?(::File.join(app['deploy_to'], "current"))
+    d = run_context.resource_collection.resources(:deploy_revision => app['id'])
+    d.restart_command do
+      execute "/etc/init.d/#{app['id']} hup"
+    end
+  end
+end
